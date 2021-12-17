9bcebe1a876fc6500ecd9d0e3e9589d80c7f982e
Ivy B <80648381+ivybergstrom@users.noreply.github.com>
2021-12-17 07:39:48 -0700

    initial commit

---
+++ src/Filters/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Filter from "./Filter.js";
+import { filterOptions } from "../data.js";
+
+const FiltersList = (props) => {
+  const renderFilter = (filter) => {
+    const isChecked = props.selectedFilters.includes(filter.key);
+    return (
+      <Filter
+        key={filter.key}
+        filter={filter}
+        isChecked={isChecked}
+        /*passes reference to toggleFilter method to this Filter componnent*/
+        toggleFilter={props.toggleFilter}
+      />
+    );
+  };
+  const filterListElements = filterOptions.map(renderFilter);
+  return (
+    <div className="filters">
+      <h5 className="filters__header">Filter By:</h5>
+      <hr />
+      <ul className="filters-list">{filterListElements}</ul>
+    </div>
+  );
+};
+
+export default FiltersList;
